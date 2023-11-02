18d1db5dda39f6c316355e625784447f55a025cf
ostandrey <ost.and.g@gmail.com>
2023-11-02 13:43:38 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <>
+      <div>Proj</div>
+      <Button />
+    </>
+  );
+}
+
+export default App;
